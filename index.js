e3d281a175cca657ca78778f382fa699c403c086
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-01-26 08:46:53 +0000

    Configurable port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('wdots');
 const app = require('./lib/app');
-const port = 9000;
+const argv = require('yargs').default('port', 9000).argv;
+const port = argv.port;
 
 app.listen(port, () => {
   debug('Listening on port %s', port);
