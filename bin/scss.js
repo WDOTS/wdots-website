e754cc69c6b1e129a759fc65f3c4d06145b23eb4
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-01-31 19:53:55 +0000

    Use node-sass package instead of cli

--- bin/scss.js
+++ bin/scss.js
@@ -1,17 +1,40 @@
 #!/usr/bin/env node
-const spawn = require('child_process').spawn;
+const sass = require('node-sass');
 const fs = require('fs');
-const nodeSassPath = './node_modules/node-sass/bin/node-sass';
-
-fs.readdir('./styles', (err, files) => {
-  files.forEach((fileName) => {
-    if (fileName.match(/\.scss$/)) {
-      const nodeSassArgs = [
-        `./styles/${fileName}`,
-        '--output', './build/css'
-      ];
-
-      spawn(nodeSassPath, nodeSassArgs);
+const mkdirp = require('mkdirp');
+const debug = require('debug')('wdots');
+const scssRegExp = /(\w*)\.scss$/;
+const buildDir = './build/css/';
+const stylesDir = './styles';
+const render = (fileNameStem) => {
+  sass.render({
+    file: `${stylesDir}/${fileNameStem}.scss`,
+    outFile: `${buildDir}/${fileNameStem}.css`
+  }, (sassErr, result) => {
+    if (sassErr) {
+      debug(sassErr);
+      return;
     }
+    fs.writeFile(`${buildDir}/${fileNameStem}.css`, result.css);
+  });
+};
+
+mkdirp(buildDir, (mkdirErr) => {
+  if (mkdirErr) {
+    debug(mkdirErr);
+    return;
+  }
+  fs.readdir(stylesDir, (readErr, files) => {
+    if (readErr) {
+      debug(readErr);
+      return;
+    }
+    files.forEach((fileName) => {
+      const fileNameStem = scssRegExp.test(fileName) && scssRegExp.exec(fileName)[1];
+
+      if (fileNameStem) {
+        render(fileNameStem);
+      }
+    });
   });
 });
